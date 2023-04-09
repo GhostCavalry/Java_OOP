67b3b9a4eaa7bbb8fdd01606dd073588bbbe184d
Ahmet İzzet Sayın <90316467+GhostCavalry@users.noreply.github.com>
2023-04-09 23:34:48 +0300

    Updated
    Some topics issues has changed

--- src/skeleton/Main.java
+++ src/skeleton/Main.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
+		Topic able = new Topic();
+		able.ElitUye();
+		
+		Topic ableD = new Topic();
+		ableD.GenelUye();
+		
 		MainAccess accessable = new MainAccess(); // Program ilk çalıştırıldığında main tarafından MainAccess classına yönlendirilerek başlar
 		accessable.Welcome();
 		
